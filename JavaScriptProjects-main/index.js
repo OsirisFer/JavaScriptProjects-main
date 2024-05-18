17e5b2cb69f6afe8ab5b332f9948b6db66fd754a
OsirisFer <88387319+OsirisFer@users.noreply.github.com>
2024-05-18 02:20:00 -0300

    aber

--- JavaScriptProjects-main/index.js
+++ JavaScriptProjects-main/index.js
@@ -5,17 +5,17 @@ const port = process.env.PORT || 3000;
 
 // Configuración para servir archivos estáticos en producción
 if (process.env.NODE_ENV === 'production') {
-  // Sirve archivos estáticos desde el directorio 'frontend/build'
-  app.use(express.static(path.join(__dirname, 'frontend/build')));
+  // Sirve archivos estáticos desde el directorio 'JavaScriptProjects-main/frontend/build'
+  app.use(express.static(path.join(__dirname, 'JavaScriptProjects-main/frontend/build')));
 
   // Ruta específica para servir 'index.html' en '/'
   app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'JavaScriptProjects-main/frontend/build', 'index.html'));
   });
 
   // Ruta comodín para servir 'index.html' en cualquier otra ruta
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'JavaScriptProjects-main/frontend/build', 'index.html'));
   });
 }
 
