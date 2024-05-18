b7053fb7f379183d7494a901c29a8224eb3b3e02
OsirisFer <88387319+OsirisFer@users.noreply.github.com>
2024-05-18 02:07:37 -0300

    json 2

--- JavaScriptProjects-main/index.js
+++ JavaScriptProjects-main/index.js
@@ -1,26 +1,52 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-if (process.env.NODE_ENV !== 'production') {
-  app.use(express.static(path.join(__dirname, 'frontend/public')));
-  
-  app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'frontend/public', 'index.html'));
-  });
-} else {
+// Configuración para servir archivos estáticos en producción
+if (process.env.NODE_ENV === 'production') {
+  // Sirve archivos estáticos desde el directorio 'frontend/build'
   app.use(express.static(path.join(__dirname, 'frontend/build')));
-  
+
+  // Ruta específica para servir 'index.html' en '/'
   app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
   });
 
+  // Ruta comodín para servir 'index.html' en cualquier otra ruta
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
   });
 }
 
+// Iniciar el servidor
 app.listen(port, () => {
   console.log(`Servidor escuchando en http://localhost:${port}`);
 });
+
+// const express = require('express');
+// const path = require('path');
+// const app = express();
+// const port = 3000;
+
+// if (process.env.NODE_ENV !== 'production') {
+//   app.use(express.static(path.join(__dirname, 'frontend/public')));
+  
+//   app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'frontend/public', 'index.html'));
+//   });
+// } else {
+//   app.use(express.static(path.join(__dirname, 'frontend/build')));
+  
+//   app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
+//   });
+
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
+//   });
+// }
+
+// app.listen(port, () => {
+//   console.log(`Servidor escuchando en http://localhost:${port}`);
+// });
