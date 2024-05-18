0e1c633598204b22e862ea0a491b2adc77d91bf0
OsirisFer <88387319+OsirisFer@users.noreply.github.com>
2024-05-18 02:14:11 -0300

    index

--- JavaScriptProjects-main/index.js
+++ JavaScriptProjects-main/index.js
@@ -23,30 +23,3 @@ if (process.env.NODE_ENV === 'production') {
 app.listen(port, () => {
   console.log(`Servidor escuchando en http://localhost:${port}`);
 });
-
-// const express = require('express');
-// const path = require('path');
-// const app = express();
-// const port = 3000;
-
-// if (process.env.NODE_ENV !== 'production') {
-//   app.use(express.static(path.join(__dirname, 'frontend/public')));
-  
-//   app.get('/', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'frontend/public', 'index.html'));
-//   });
-// } else {
-//   app.use(express.static(path.join(__dirname, 'frontend/build')));
-  
-//   app.get('/', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
-//   });
-
-//   app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
-//   });
-// }
-
-// app.listen(port, () => {
-//   console.log(`Servidor escuchando en http://localhost:${port}`);
-// });
