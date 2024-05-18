861c7589c88baae7bb69ea7117da3794e101e109
OsirisFer <88387319+OsirisFer@users.noreply.github.com>
2024-05-18 13:50:26 -0300

    si no funca arranco con otro

--- JavaScriptProjects-main/index.js
+++ JavaScriptProjects-main/index.js
@@ -3,21 +3,18 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Configuración para servir archivos estáticos en producción
-if (process.env.NODE_ENV === 'production') {
-  // Sirve archivos estáticos desde el directorio 'JavaScriptProjects-main/frontend/build'
-  app.use(express.static(path.join(__dirname, 'JavaScriptProjects-main/frontend/build')));
+// Configuración para servir archivos estáticos
+app.use(express.static(path.join(__dirname, 'JavaScriptProjects-main/frontend/build')));
 
-  // Ruta específica para servir 'index.html' en '/'
-  app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'JavaScriptProjects-main/frontend/build', 'index.html'));
-  });
+// Ruta específica para servir 'index.html' en '/'
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'JavaScriptProjects-main/frontend/build', 'index.html'));
+});
 
-  // Ruta comodín para servir 'index.html' en cualquier otra ruta
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'JavaScriptProjects-main/frontend/build', 'index.html'));
-  });
-}
+// Ruta comodín para servir 'index.html' en cualquier otra ruta
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'JavaScriptProjects-main/frontend/build', 'index.html'));
+});
 
 // Iniciar el servidor
 app.listen(port, () => {
